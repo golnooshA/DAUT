548c61f16e3a0d90a6782be904becc8e1d0a552c
Golnoosh Asady <golnoosh.asady@gmail.com>
2024-12-19 18:49:58 +0100

    Initial commit

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ def main():
     PATH_INPUT = './dataset/UIEB/input'
     PATH_DEPTH = './DPT/output_monodepth/UIEB_Changed'
     PATH_GT = './dataset/UIEB/GT/'
-    SAVE_DIR = './save_model/'
+    SAVE_DIR = '/content/drive/My Drive/My_Datasets/save_model/'
 
     os.makedirs(SAVE_DIR, exist_ok=True)
 
