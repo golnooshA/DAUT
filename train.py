33ac40d9cb944042bc29c83f87b63d30313d3d14
Golnoosh Asady <golnoosh.asady@gmail.com>
2024-12-20 15:29:23 +0100

    test chnages

--- train.py
+++ train.py
@@ -166,7 +166,8 @@ import numpy as np
 PATH_INPUT = './dataset/UIEB/input'
 PATH_DEPTH = './DPT/output_monodepth/UIEB/'
 PATH_GT = './dataset/UIEB/GT/'
-SAVE_DIR = 'C:/Users/golno/OneDrive/Desktop/Depth-Aware-U-shape-Transformer/save_model/'
+# SAVE_DIR = 'C:/Users/golno/OneDrive/Desktop/Depth-Aware-U-shape-Transformer/save_model/'
+SAVE_DIR = '/content/drive/My Drive/My_Datasets/save_model/'
 
 os.makedirs(SAVE_DIR, exist_ok=True)
 
